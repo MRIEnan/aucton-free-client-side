5e85322838a5b7ba4708035652e72ac708f2f2e1
Abdul Al Joboyer Chy <55484176+Aljoboyer@users.noreply.github.com>
2022-01-03 14:48:37 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,24 +12,33 @@ import BookmakerHome from './Pages/Bookmaker/BookmakerHome/BookmakerHome';
 import AddAuction from './Pages/Bookmaker/AddAuction/AddAuction';
 import ManageAuction from './Pages/Bookmaker/ManageAuction/ManageAuction';
 import SeeWhoBid from './Pages/Bookmaker/SeeWhoBid/SeeWhoBid';
+import Login from './Pages/Shared/Login/Login';
+import SignUp from './Pages/Shared/SignUp/SignUp';
+import AuthProvider from './Context/AuthProvider/AuthProvider';
+import AuctioneerNav from './Pages/Auctioneer/AuctioneerHome/AuctioneerNav';
 import BidDetails from './Pages/Auctioneer/BidDetails/BidDetails';
-
 function App() {
   return (
     <div className="container-fluid">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<AuctioneerHome/>}/>
-         
+       <AuthProvider>
 
-            <Route path="/BookMakerDashboard" element={<BookMakerDashboard/>}>
-                <Route path="/BookMakerDashboard" element={<BookmakerHome/>}/>
-                <Route path="/BookMakerDashboard/AddAuction" element={<AddAuction/>}/>
-                <Route path="/BookMakerDashboard/ManageAuction" element={<ManageAuction/>}/>
-                <Route path="/BookMakerDashboard/SeeWhoBid" element={<SeeWhoBid/>}/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
+<BrowserRouter>
+  <AuctioneerNav></AuctioneerNav>
+  <Routes>
+    <Route path="/" element={<AuctioneerHome />} />
+    <Route path="/BidDetails/:id" element={<BidDetails />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<SignUp />} />
+
+    <Route path="/BookMakerDashboard" element={<BookMakerDashboard />}>
+      <Route path="/BookMakerDashboard" element={<BookmakerHome />} />
+      <Route path="/BookMakerDashboard/AddAuction" element={<AddAuction />} />
+      <Route path="/BookMakerDashboard/ManageAuction" element={<ManageAuction />} />
+      <Route path="/BookMakerDashboard/SeeWhoBid/:id" element={<SeeWhoBid />} />
+    </Route>
+  </Routes>
+</BrowserRouter>
+</AuthProvider>
     </div>
   );
 }
