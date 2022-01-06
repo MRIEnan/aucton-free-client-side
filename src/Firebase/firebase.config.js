d91c2041f834928edbfaf8b635466c6c45462e68
Mainur Rahman <enan.mainur@gmail.com>
2022-01-07 00:07:24 +0600

    timer configured

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDA8BEfgtpqkVxRiO_X5Gf7bhsrGKhBqiU",
-  authDomain: "online-free-auction.firebaseapp.com",
-  projectId: "online-free-auction",
-  storageBucket: "online-free-auction.appspot.com",
-  messagingSenderId: "593509727657",
-  appId: "1:593509727657:web:05b2dd1f9b593b20e198b8"
-  };
-  export default firebaseConfig;
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomaincom,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucketappspot.com,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId
+};
+export default firebaseConfig;
   
